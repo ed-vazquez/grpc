081716bddad17bedb2df301444af5f4136ad06ef
Eduardo Vazquez <evazquez@vazqueztech.com>
2023-01-29 12:02:36 -0500

    wynw

--- lib/grpc/client/adapters/gun.ex
+++ lib/grpc/client/adapters/gun.ex
@@ -90,13 +90,11 @@ defmodule GRPC.Client.Adapters.Gun do
 
   defp open(host, port, open_opts) do
     # note
-    IO.inspect(open_opts)
-      open_opts[:tcp_opts] = if(System.fetch_env!("ECTO_IPV6") == "true") do
-          [:inet6 | open_opts[:tcp_opts]]
-      else
-        open_opts[:tcp_opts]
-      end
-      IO.inspect(open_opts)
+    open_opts = case System.fetch_env!("ECTO_IPV6") do
+      "true" -> open_opts = update_in(open_opts, [:tcp_opts], [:inet6 | open_opts.tcp_opts])
+      _ -> open_opts
+    end
+
       :gun.open(String.to_charlist(host), port, open_opts)
   end
 
